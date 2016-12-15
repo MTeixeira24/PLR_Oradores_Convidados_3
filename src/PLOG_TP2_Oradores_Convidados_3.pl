8f35fe69f1417a909a794e5fabefe208cc9c140c
MTeixeira <miguelteixeira@hotmail.com>
2016-12-15 11:12:50 +0000

    Reformulated the code. Added some hardcoded inputs. Gender restrictions

--- src/PLOG_TP2_Oradores_Convidados_3.pl
+++ src/PLOG_TP2_Oradores_Convidados_3.pl
@@ -1,24 +1,40 @@
 :-use_module(library(clpfd)).
 
-test :- write('Hello world').
-
-distance([_,_|[]],0).
-distance([A,B,C|List], Distance):-
-        Aux #= abs(B-A)/abs(C-B),
-        Distance #= abs(B-A) + DR,
-        Aux #> 0.8,
-        Aux #< 1.2,
-        distance([B,C|List],DR).
-
-
-
+listGenders([S|Speakers], [G|Genders],SpeakerInfo) :-
+        element(S,SpeakerInfo, _-L),
+        element(3,L,G),
+        listGenders(Speakers, Genders, SpeakerInfo).
+listGenders([],[],_).
 guest_speakers(NTalks, Budget, ChoosenSubjects) :-
-       Subjects = [s1-1,s2-2,s3-3,s4-4,s5-5,s6-6,s7-7,s8-8,s9-9],
-       length(ChoosenSubjects, NTalks),
-       domain(ChoosenSubjects,1,9),
-       all_distinct(ChoosenSubjects),
-       length(SortedSubjects, NTalks),
-       length(Aux, NTalks),
-       sorting(ChoosenSubjects, Aux, SortedSubjects),
-       distance(SortedSubjects,Distance),
-       labeling([maximize(Distance)],ChoosenSubjects).
+       length(Speakers, NTalks),
+       SubjectSpeakers = [
+                            t1-[1,2,3],
+                            t2-[6],
+                            t3-[2,4,5,6],
+                            t4-[1,4],
+                            t5-[2,3,5],
+                            t6-[5,6],
+                            t7-[3],
+                            t8-[2,5],
+                            t9-[3,4,5]
+                         ],
+       SpeakerInfo = [
+                        1-['name','USA',0,5,1], %%Id-[Name,Country,Gender,Nights,FirstClass]
+                        2-['name','France',1,1,0], %% 0 = Male, 1 = Female
+                        3-['name','Spain',1,3,1],
+                        4-['name','Germany',0,5,0],
+                        5-['name','Poland',0,2,0],
+                        6-['name','China',1,5,0]
+                        ],
+        CountriesDistance = [
+                            'Spain'-2,
+                            'France'-3,
+                            'Poland'-5,
+                            'Germany'-5,
+                            'China'-8 
+                             ],
+        domain(Speakers, 1, 6),
+        all_distinct(Speakers),
+        listGenders(Speakers, Genders,SpeakerInfo),
+        global_cardinality(Genders,[0-Males,1-Females]),
+        Males #= Females.
