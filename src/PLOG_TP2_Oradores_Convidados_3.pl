3735671573affafbe71513bb85f84e1ab590a748
MTeixeira <miguelteixeira@hotmail.com>
2016-12-15 09:49:23 +0000

    Subject selection

--- src/PLOG_TP2_Oradores_Convidados_3.pl
+++ src/PLOG_TP2_Oradores_Convidados_3.pl
@@ -2,6 +2,23 @@
 
 test :- write('Hello world').
 
-guest_speakers(NTalks, Budget) :-
-        %%dominio
-        test.
+distance([_,_|[]],0).
+distance([A,B,C|List], Distance):-
+        Aux #= abs(B-A)/abs(C-B),
+        Distance #= abs(B-A) + DR,
+        Aux #> 0.8,
+        Aux #< 1.2,
+        distance([B,C|List],DR).
+
+
+
+guest_speakers(NTalks, Budget, ChoosenSubjects) :-
+       Subjects = [s1-1,s2-2,s3-3,s4-4,s5-5,s6-6,s7-7,s8-8,s9-9],
+       length(ChoosenSubjects, NTalks),
+       domain(ChoosenSubjects,1,9),
+       all_distinct(ChoosenSubjects),
+       length(SortedSubjects, NTalks),
+       length(Aux, NTalks),
+       sorting(ChoosenSubjects, Aux, SortedSubjects),
+       distance(SortedSubjects,Distance),
+       labeling([maximize(Distance)],ChoosenSubjects).
