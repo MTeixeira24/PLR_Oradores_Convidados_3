76f7fce99152e40b584c7db49e8798528ca47176
MTeixeira <miguelteixeira@hotmail.com>
2016-12-20 22:57:40 +0000

    Updated tons of stuff

--- src/PLOG_TP2_Oradores_Convidados_3.pl
+++ src/PLOG_TP2_Oradores_Convidados_3.pl
@@ -1,100 +1,108 @@
 :-use_module(library(clpfd)).
+:-use_module(library(lists)).
 
 
-listGenders([S|Speakers], [G|Genders],SpeakerInfo) :-
-        table([[S,_,G,_,_]],SpeakerInfo),
-        listGenders(Speakers, Genders, SpeakerInfo).
+getGenders([LS|SpeakerInfo], [G|SpeakerInfoGenderList]) :-
+        nth1(2, LS, G),
+        getGenders(SpeakerInfo, SpeakerInfoGenderList).
+getGenders([],[]).
+
+listGenders([S|Speakers], [G|Genders],SpeakerInfoGenderList) :-
+        element(S, SpeakerInfoGenderList, G1),
+        G #= G1,
+        listGenders(Speakers, Genders, SpeakerInfoGenderList).
 listGenders([],[],_).
 
-checkSpeeches(Speakers, [CS|ChoosenSubjects], SubjectSpeakers) :-
-        table([LS], SubjectSpeakers),
-        element(1,LS, CS),
-        element(_,LS,A),
-        element(_,Speakers, B),
-        B#=A,
+checkSpeeches([S|Speakers], [CS|ChoosenSubjects], SubjectSpeakers) :-
+        element(S, SubjectSpeakers, CS),
         checkSpeeches(Speakers, ChoosenSubjects, SubjectSpeakers).
 checkSpeeches(_,[],_).
 
 getDistanceList([C|ChoosenSubjects], SubjectDistance, [Distance|DistanceList]) :-
-        getSubDistances(C, ChoosenSubjects, SubjectDistance,Distances),
-        sum(Distances, #=, Distance),
+        member(AuxLength, SubjectDistance),
+        length(AuxLength, N),
+        N1 is N - 1,
+        length(Aux, N1),
+        Distances = [C|Aux],
+        table([Distances], SubjectDistance),
+        getSubDistances(ChoosenSubjects, Distances, DAux),
+        sum(DAux, #=, Distance),
         getDistanceList(ChoosenSubjects, SubjectDistance, DistanceList).
 getDistanceList([],_,[]).
 
-getSubDistances(C, [CC|ChoosenSubjects], SubjectDistance,[D|Distances]) :-
-        table([[C,CC,D]], SubjectDistance),
-        getSubDistances(C, ChoosenSubjects, SubjectDistance, Distances).
-getSubDistances(_,[],_,[]).
+getSubDistances([Subject|ChoosenSubjects], Distances, [D|List]) :-
+        Aux #= Subject + 1,
+        element(Aux, Distances, D),
+        getSubDistances(ChoosenSubjects, Distances, List).
+getSubDistances([],_,[]).
+
+isSorted([_|[]]).
+isSorted([A,B|List]) :-
+        A #> B,
+        isSorted([B|List]).
+
 
 guest_speakers(NTalks, Budget, Speakers, ChoosenSubjects) :-
-       length(Speakers, NTalks),
-       length(ChoosenSubjects, NTalks),
-       SubjectSpeakers = [ 
-                            [1,1,2,3], %%Subject Id, Speakers
-                            [2,6],
-                            [3,2,4,5,6],
-                            [4,1,4],
-                            [5,2,3,5],
-                            [6,5,6],
-                            [7,3],
-                            [8,2,5],
-                            [9,3,4,5]
+       
+        %%Lists
+        %%Speakers >> Topics
+        
+        SubjectSpeakers = [ 
+                            2, %%Index being the speaker and the value the topic
+                            1,
+                            5,
+                            4,
+                            3,
+                            2,
+                            9,
+                            7,
+                            3,
+                            6,
+                            4,
+                            5
                          ],
        SubjectDistance = [
-        [1,2,1],  %%Subject 1, Subject 2, Distance
-        [1,3,1],
-        [1,4,1],
-        [1,5,1],
-        [1,6,1],
-        [1,7,1],
-        [1,8,1],
-        [1,9,1],
-        [2,3,71],
-        [2,4,88],
-        [2,5,24],
-        [2,6,25],
-        [2,7,60],
-        [2,8,24],
-        [2,9,84],
-        [3,4,62],
-        [3,5,37],
-        [3,6,15],
-        [3,7,17],
-        [3,8,61],
-        [3,9,63],
-        [4,5,55],
-        [4,6,82],
-        [4,7,57],
-        [4,8,21],
-        [4,9,37],
-        [5,6,64],
-        [5,7,93],
-        [5,8,82],
-        [5,9,34],
-        [6,7,45],
-        [6,8,14],
-        [6,9,26],
-        [7,8,21],       
-        [7,9,62],
-        [8,9,1500000]
+        [1,-1,29,60,3,95,55,29,39,12],%%Line is the subject, Column is other subject, contents are the distance
+        [2,-1,-1,71,88,24,25,60,24,84],
+        [3,0,0,0,62,37,15,17,61,63],
+        [4,-1,9,-1,-1,55,82,57,21,37],
+        [5,0,0,0,0,0,64,93,82,34],
+        [6,0,0,0,0,0,0,45,14,26],
+        [7,0,0,0,0,0,0,0,21,62],
+        [8,0,0,0,0,0,0,0,0,150],
+        [9,0,0,0,0,0,0,0,0,0]
                          ],
        SpeakerInfo = [
-                        [1,2,0,5,1], %%Id,Distance,Gender,Nights,FirstClass
-                        [2,3,1,1,0], %% 0 = Male, 1 = Female
-                        [3,5,1,3,1],
-                        [4,5,0,5,0],
-                        [5,3,0,2,0],
-                        [6,8,1,5,0]
+                        [2,0,5,1], %%Distance,Gender,Nights,FirstClass
+                        [3,1,1,0], %% 0 = Male, 1 = Female
+                        [5,1,3,1], %%Index number is the id of the speaker
+                        [5,0,5,0],
+                        [3,0,2,0],
+                        [8,1,5,0],
+                        [2,1,5,0],
+                        [9,0,1,1],
+                        [8,1,5,0],
+                        [5,0,3,0],
+                        [4,1,5,1],
+                        [2,1,2,0]
                         ],
+        length(Speakers, NTalks),
+        length(ChoosenSubjects, NTalks),
+        length(Genders, NTalks),
         domain(Speakers, 1, 6),
         domain(ChoosenSubjects,1,9),
+        domain(Genders, 0, 1),
         all_distinct(Speakers),
-        listGenders(Speakers, Genders,SpeakerInfo),
+        all_distinct(ChoosenSubjects),
         global_cardinality(Genders,[0-Males,1-Females]),
         Males #= Females,
-        all_distinct(ChoosenSubjects),
+        getGenders(SpeakerInfo, SpeakerInfoGenderList),
+        listGenders(Speakers, Genders, SpeakerInfoGenderList),
+        isSorted(ChoosenSubjects),
         checkSpeeches(Speakers, ChoosenSubjects, SubjectSpeakers),
         getDistanceList(ChoosenSubjects, SubjectDistance, DistanceList),
-        sum(DistanceList, #=, TotalDistance),
-        labeling([maximize(TotalDistance)], ChoosenSubjects)
-        .
+        sum(DistanceList, #= ,TotalDistance),
+        labeling([maximize(TotalDistance)], ChoosenSubjects),
+        write(TotalDistance).
+
+
